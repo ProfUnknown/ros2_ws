9e5b05ae73dbd61fd79d95e683dda066db0c1189
akira249 <akirakenji78@gmail.com>
2025-09-13 14:25:01 -0400

    feat: menggunakan interface

--- src/cpp_pubsub/src/publisher_member_function.cpp
+++ src/cpp_pubsub/src/publisher_member_function.cpp
@@ -16,22 +16,32 @@ class MinimalPublisher : public rclcpp::Node
         : Node ("minimal_publisher"), count_(0)
         {
             // publisher_ = this ->create_publisher <std_msgs::msg::String>("topic",10); //Sebelum pakai interface
-            
+            publisher_ = this->create_publisher<tutorial_interfaces::msg::Num>("topic", 10);  // CHANGE
             timer_ = this->create_wall_timer(
                 500ms, std::bind(&MinimalPublisher::timer_callback, this)
             );
         }
     
         private:
+        // void timer_callback()
+        // {
+        //     auto message = std_msgs::msg::String();
+        //     message.data = "Hello, world!" + std::to_string(count_++);
+        //     RCLCPP_INFO(this->get_logger(), "Publishing : '%s'", message.data.c_str());
+        //     publisher_->publish(message);
+        // } // sebelum menggunakan interface
         void timer_callback()
         {
-            auto message = std_msgs::msg::String();
-            message.data = "Hello, world!" + std::to_string(count_++);\
-            RCLCPP_INFO(this->get_logger(), "Publishing : '%s'", message.data.c_str());
+            auto message = tutorial_interfaces::msg::Num();                                   // CHANGE
+            message.num = this->count_++;                                                     // CHANGE
+            RCLCPP_INFO_STREAM(this->get_logger(), "Publishing: '" << message.num << "'");    // CHANGE
             publisher_->publish(message);
         }
+  
+
         rclcpp::TimerBase::SharedPtr timer_;
-        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+        // rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_; // Sebelum menggunakan interface
+        rclcpp::Publisher<tutorial_interfaces::msg::Num>::SharedPtr publisher_; //CHANGE
         size_t count_;
 };
 
--- src/cpp_pubsub/src/subscriber_member_function.cpp
+++ src/cpp_pubsub/src/subscriber_member_function.cpp
@@ -3,8 +3,8 @@
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
-
+// #include "std_msgs/msg/string.hpp" // Sebelum menggunakan interface
+#include "tutorial_interfaces/msg/num.hpp" //CHANGE
 using std::placeholders::_1;
 
 class MinimalSubscriber : public rclcpp::Node
@@ -13,16 +13,28 @@ public:
   MinimalSubscriber()
   : Node("minimal_subscriber")
   {
-    subscription_ = this->create_subscription<std_msgs::msg::String>(
+    // subscription_ = this->create_subscription<std_msgs::msg::String>(
+    //   "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+    //   //Sebelum menggunakan interface
+
+    subscription_ = this->create_subscription<tutorial_interfaces::msg::Num>(    // CHANGE
       "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+    
   }
 
 private:
-  void topic_callback(const std_msgs::msg::String & msg) const
+//   void topic_callback(const std_msgs::msg::String & msg) const
+//   {
+//     RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
+//   }
+//   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+// }; //sebelum menggunakan interface
+
+void topic_callback(const tutorial_interfaces::msg::Num & msg) const  // CHANGE
   {
-    RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
+    RCLCPP_INFO_STREAM(this->get_logger(), "I heard: '" << msg.num << "'");     // CHANGE
   }
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+  rclcpp::Subscription<tutorial_interfaces::msg::Num>::SharedPtr subscription_;  // CHANGE
 };
 
 int main(int argc, char * argv[])
